37cf19cdc3d190b7b482a6d0bb6ab1db14bf3a0d
akagiyuu <khanghuynh0245@gmail.com>
2026-05-28 20:22:40 +0700

    feat: init color_eyre

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
+use std::env;
+
 mod config;
 mod feature;
 mod transport;
 
 #[tokio::main]
 async fn main() -> color_eyre::Result<()> {
+    if env::var("NO_COLOR") == Err(env::VarError::NotPresent) {
+        color_eyre::install()?;
+    } else {
+        color_eyre::config::HookBuilder::new()
+            .theme(color_eyre::config::Theme::new())
+            .install()?;
+    }
+
     transport::http::run().await
 }
