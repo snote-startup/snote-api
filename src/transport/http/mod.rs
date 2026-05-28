55f3b4a09e6c723e4843dd353f088ce87e859086
akagiyuu <khanghuynh0245@gmail.com>
2026-05-28 20:12:49 +0700

    refactor(http): extract router creating into function

--- src/transport/http/mod.rs
+++ src/transport/http/mod.rs
@@ -8,15 +8,20 @@ use tokio::net::TcpListener;
 
 use crate::{config::CONFIG, transport::http::state::ApiState};
 
-pub async fn run() -> color_eyre::Result<()> {
+async fn build() -> color_eyre::Result<Router> {
     let state = ApiState::new().await?;
-    let app = Router::new().with_state(state);
+
+    Ok(Router::new().with_state(state))
+}
+
+pub async fn run() -> color_eyre::Result<()> {
+    let api = build().await?;
 
     let listener = TcpListener::bind(SocketAddr::new([0, 0, 0, 0].into(), CONFIG.port)).await?;
 
     tracing::info!("Listening on port {}", CONFIG.port);
 
-    axum::serve(listener, app).await?;
+    axum::serve(listener, api).await?;
 
     Ok(())
 }
