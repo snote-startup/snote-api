b8cc02725f3c2c933137938187779c8f4e28c241
akagiyuu <khanghuynh0245@gmail.com>
2026-06-06 08:29:06 +0700

    chore: format

--- src/feature/project/repository/mod.rs
+++ src/feature/project/repository/mod.rs
@@ -2,7 +2,7 @@ mod chat_message;
 mod project;
 mod transcript_segment;
 
-pub use project::*;
-pub use transcript_segment::*;
 #[allow(unused)]
 pub use chat_message::*;
+pub use project::*;
+pub use transcript_segment::*;
