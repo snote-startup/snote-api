504baf2acd7a1280de898ac110d072b0f939895c
akagiyuu <khanghuynh0245@gmail.com>
2026-06-06 07:58:04 +0700

    refactor(database): simplify table names

--- migrations/20260605113101_create_chat_message.sql
+++ migrations/20260605113101_create_chat_message.sql
@@ -1,12 +1,12 @@
-CREATE TYPE conversation_role AS ENUM (
+CREATE TYPE chat_role AS ENUM (
     'user',
     'assistance'
 );
 
-CREATE TABLE IF NOT EXISTS llm_conversation_histories(
+CREATE TABLE IF NOT EXISTS chat_messages(
     id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
     project_id uuid NOT NULL REFERENCES projects(id),
-    role conversation_role NOT NULL,
+    role chat_role NOT NULL,
     content text NOT NULL,
     created_at timestamptz NOT NULL DEFAULT now()
 );
