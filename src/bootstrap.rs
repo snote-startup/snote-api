c8bfdb03c1aaff3975fdbfe410c97fb8b10fe975
akagiyuu <khanghuynh0245@gmail.com>
2026-06-16 21:06:32 +0700

    refactor: move prefix to routes file

--- src/bootstrap.rs
+++ src/bootstrap.rs
@@ -18,8 +18,8 @@ pub async fn run() -> color_eyre::Result<()> {
     let state = Arc::new(ApiState::new(config).await?);
     let app = Router::new()
         .route("/health", routing::get(health::health))
-        .nest("/auth", auth::routes())
-        .nest("/project", project::routes())
+        .merge(auth::routes())
+        .merge(project::routes())
         .layer(middleware::cors(&origins))
         .layer(TraceLayer::new_for_http())
         .merge(doc::build())
--- src/feature/auth/routes.rs
+++ src/feature/auth/routes.rs
@@ -6,8 +6,8 @@ use crate::{feature::auth::handler, shared::ApiState};
 
 pub fn routes() -> Router<Arc<ApiState>> {
     Router::new()
-        .route("/register", routing::post(handler::register))
-        .route("/login", routing::post(handler::login))
-        .route("/refresh", routing::post(handler::refresh))
-        .route("/me", routing::get(handler::me))
+        .route("/auth/register", routing::post(handler::register))
+        .route("/auth/login", routing::post(handler::login))
+        .route("/auth/refresh", routing::post(handler::refresh))
+        .route("/auth/me", routing::get(handler::me))
 }
