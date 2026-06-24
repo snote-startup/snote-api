0426a631db1c4f24ba60f135941db98bd9ec2290
akagiyuu <khanghuynh0245@gmail.com>
2026-06-24 20:04:31 +0700

    feat(infra/payment): add constructor

--- src/infra/payment/mod.rs
+++ src/infra/payment/mod.rs
@@ -1,1 +1,21 @@
 mod dto;
+
+const API_URL: &str = "https://api-merchant.payos.vn";
+
+pub struct PayOSClient {
+    client_id: String,
+    api_key: String,
+    checksum_key: String,
+    http: reqwest::Client,
+}
+
+impl PayOSClient {
+    pub fn new(client_id: String, api_key: String, checksum_key: String) -> Self {
+        Self {
+            client_id,
+            api_key,
+            checksum_key,
+            http: reqwest::Client::new(),
+        }
+    }
+}
